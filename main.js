5a349a55a23e0d9740f38c8e1e035f180ce9a4ac
christocarr <christocarr@outlook.com>
2017-09-23 12:53:18 +0100

    mobile navigation

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+
+
+function menuFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "active") {
+        x.className += "responsive";
+    } else {
+        x.className = "active";
+    }
+}
+
+
 var crt = document.getElementById('myChart');
